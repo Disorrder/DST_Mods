d615d2f400c52193d288a3104b1d5377af32e8c9
Disorder <disorrder@gmail.com>
2015-01-25 03:24:28 +0300

    Фикс пробелов в раннере

--- Mods/runDST.cmd
+++ Mods/runDST.cmd
@@ -1,6 +1,6 @@
 @echo off
-for /f "tokens=3" %%i in ('reg query HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam /v InstallPath 2^>nul') do set steamPath=%%i
-for /f "tokens=3" %%i in ('reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam /v InstallPath 2^>nul') do set steamPath=%%i
+for /f "tokens=2*" %%A in ('reg query HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam /v InstallPath 2^>nul') do set steamPath=%%B
+for /f "tokens=2*" %%A in ('reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam /v InstallPath 2^>nul') do set steamPath=%%B
 set gamePath=%steamPath%\SteamApps\common\Don't Starve Together Beta
 set modPath=%gamePath%\mods\Character_Michael
 echo(
@@ -10,4 +10,6 @@ echo(
 ::mkdir "%modPath%"
 xcopy /s /Y /D /Q Character_Michael "%modPath%"
 
+pause
+exit
 start steam://rungameid/322330
