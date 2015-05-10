4619f0931ba2d605074d26516565e0857e3dcb7b
Disorrder <disorrder@gmail.com>
2015-05-10 23:50:55 +0300

    батник...
    Попытка разобраться, почему не работает на 7
    команда start не работает в терминале?!

--- Mods/runDST_debug.cmd
+++ Mods/runDST_debug.cmd
@@ -3,13 +3,15 @@
 @echo off
 for /f "tokens=2*" %%A in ('reg query HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam /v InstallPath 2^>nul') do set steamPath=%%B
 for /f "tokens=2*" %%A in ('reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam /v InstallPath 2^>nul') do set steamPath=%%B
+:: -- ?
+echo Steam path: %steamPath%
 set gamePath=%steamPath%\SteamApps\common\Don't Starve Together Beta
+:: -- ?
+echo Game path: %gamePath%
 set modName=Character_Michael
 set modPath=%gamePath%\mods\%modName%
-echo(
-echo Copy files to %modPath%
-echo(
-XCOPY /s /Y /D /Q Character_Michael "%modPath%"
+echo Mod path: %modPath%
+pause
 
 :: --- checking deleted files ---
 setlocal enabledelayedexpansion
@@ -17,15 +19,30 @@ for /R "%modPath%" %%F in (*) do (
     set path=%%F
     set file=%modName%!path:%modPath%=!
     set ext=%%~xF
+    echo file: !file! [!ext!]
+    :: ext = extension
     :: NEQ = not equal
+    if exist !file! (
+        echo Exists: !file!
+    )
     if !ext! NEQ .zip (
         if not exist !file! (
             echo Remove: !file!
+            pause
             DEL "%%F"
         )
     )
 )
 endlocal
+echo Deleted!
+pause
+
+echo(
+echo Copy files to %modPath%
+echo(
+XCOPY /s /Y /D /Q Character_Michael "%modPath%"
+echo Copied!
+pause
 
 :: --- Force start ---
 set modsettingsFile=%gamePath%\mods\modsettings.lua
@@ -36,4 +53,8 @@ if errorlevel 1 (
     ECHO %enableStr% >> "%modsettingsFile%"
 )
 
+pause
 START "" steam://rungameid/322330
+:: -- ?
+echo Game started (322330)
+pause
